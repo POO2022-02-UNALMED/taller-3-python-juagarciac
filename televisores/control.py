8cb7f12accee588eada0f16dd4a77cd72cced1fa
juagarciac <111471638+juagarciac@users.noreply.github.com>
2022-08-28 16:15:13 -0500

    ff

--- televisores/control.py
+++ televisores/control.py
@@ -9,16 +9,16 @@ class Control:
     def setTv(self,tv):
         self.__tv=tv
     def turnOn(self):
-        self.__tv.turnOn
+        self.__tv.turnOn()
     def turnOff(self):
-        self.__tv.turnOff
+        self.__tv.turnOff()
     def setCanal(self,canal):
         self.__tv.setCanal(canal)
     def volumenUp(self):
-        self.__tv.volumenUp
+        self.__tv.volumenUp()
     def canalUp(self):
-        self.__tv.canalUp
+        self.__tv.canalUp()
     def volumenDown(self):
-        self.__tv.volumenDown
+        self.__tv.volumenDown()
     def canalDown(self):
-        self.__tv.canalDown
+        self.__tv.canalDown()
